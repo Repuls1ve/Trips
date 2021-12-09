52464f78639ec108530a805162012fcc2210a017
Repuls1ve <repulsive@list.ru>
2021-12-09 22:04:41 +0300

    animated header sidebar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { HomeComponent } from './views/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slide', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-100%)',
+        }),
+        animate(350)
+      ]),
+      transition(':leave', [
+        animate(350, style({
+          transform: 'translateY(-100%)'
+        }))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   sidebarClosed = true
